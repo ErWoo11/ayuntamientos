97d88831281489a86add076291e2cb364778f316
ErWoo11 <latribu_woo11@hotmail.com>
2026-04-24 20:42:38 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+  const firebaseConfig = {
+  apiKey: "AIzaSyBzidosSZRxKmjMIrg0zAjYRt_rbohcHLU",
+  authDomain: "saas-45027.firebaseapp.com",
+  projectId: "saas-45027",
+  storageBucket: "saas-45027.firebasestorage.app",
+  messagingSenderId: "117144809845",
+  appId: "1:117144809845:web:83153cf3aa6bc97851233c"
+  };
